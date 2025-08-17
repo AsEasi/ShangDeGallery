34be7377975a2f60b7b8366b82e1ef2065a07226
Easi-GH <easi.guo@outlook.com>
2025-08-17 18:08:43 +0800

    Update test version 2508171800

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "CHAOS-ShangdeGallery-0.1.0";
+const contentToCache = [
+    "Build/WebGL_2508171800.loader.js",
+    "Build/WebGL_2508171800.framework.js.unityweb",
+    "Build/WebGL_2508171800.data.unityweb",
+    "Build/WebGL_2508171800.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
